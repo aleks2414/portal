54e4a350b6211287c810f5172b262ba17a3ad58b
aleks2414 <alejandroromo14@gmail.com>
2018-01-15 12:23:32 -0600

    fotos

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -47,6 +47,7 @@ class AppointmentsController < ApplicationController
   def update
     respond_to do |format|
       if @appointment.update(appointment_params)
+        ProviderMailer.appointment_confirmation(@appointment).deliver
         format.html { redirect_to provider_appointment_path(@provider, @appointment), notice: 'Appointment was successfully updated.' }
         format.json { render :show, status: :ok, location: @appointment }
       else
